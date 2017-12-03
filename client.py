53ca6fead614e060c9472dbc69ff38769ab0512d
TheBlasianTJ <tjdosunmu@gmail.com>
2017-12-03 03:53:48 -0500

    Added keyboard listening

--- client.py
+++ client.py
@@ -1,27 +1,48 @@
 # keyboard simulator
+import os
 import pyautogui
 import socket
 import sys
+import webbrowser
+
+from pynput import keyboard
+
+filename = 'index.html'
 
 
-def register_device(host, port):
+def register(host, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((host, port))
 
     return s
 
 
-if __name__ == '__main__':
-    if len(sys.argv) == 3:
-        host = sys.argv[1]
-        port = int(sys.argv[2])
-        s = register_device(host, port)
-        s.sendall('Connected to Device')
+def calibrate():
+    webbrowser.open('file://' + os.path.realpath(filename))
 
-        while 1:
+
+if __name__ == '__main__':
+    try:
+        if len(sys.argv) == 3:
+            control = keyboard.Controller()
+            host = sys.argv[1]
+            port = int(sys.argv[2])
+            s = register(host, port)
+
+            s.sendall('Connected to Device')
+
             data = s.recv(1024)
-            print("data received: " + repr(data))
-    else:
-        print('ERROR: Incorrect number of arguments used. ' +
-              'You must pass in the HOST and PORT of the endpoint to connect to\n' +
-              'Example: python client.py host_name port_number')
+            print(repr(data))
+            calibrate()
+
+            while 1:
+                data = s.recv(1024)
+
+                print("data received: " + repr(data))
+                break
+        else:
+            print('ERROR: Incorrect number of arguments used. ' +
+                  'You must pass in the HOST and PORT of the endpoint to connect to\n' +
+                  'Example: python client.py host_name port_number')
+    except:
+        s.close()
--- server.py
+++ server.py
@@ -1,34 +1,107 @@
 # signal mux
+# https://pypi.python.org/pypi/pynput
+
 import socket
 import sys
+import traceback
+
+from pynput import keyboard
+
+kb = keyboard.Controller()
 
 
 class Device:
     def __init__(self, addr, conn):
+        self.addr = addr
+        self.conn = conn
+
+        self.tl = None
+        self.bl = None
+        self.br = None
+
+    def add_tl():
+        pass
+
+    def add_bl():
+        pass
+
+    def add_br():
         pass
 
 
-# devices = {}
-
 # devices[addr] = Device(addr, conn)
+devices = {}
+# save addr of focuesd device here
+focused = None
 
+
+def calibrate(device):
+    """
+        on each space press, snap an image.
+        process the image using gVAPI 
+        store the bounds in correct position 
+    """
+    pass
+
+
+def on_press(key):
+    kb.press(key)
+    try:
+        print key
+        # if focused:
+        #     conn = devices[focused]
+        # conn.sendall(key.char)
+        # print('alphanumeric key {0} pressed'.format(
+        #     key.char))
+    except AttributeError:
+        print('special key {0} pressed'.format(
+            key))
+        if focused:
+            conn = devices[focused]
+            conn.sendall(key.char)
+
+
+def on_release(key):
+    kb.release(key)
+    print('{0} released'.format(
+        key))
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+
+# Collect events until released
 
 if __name__ == '__main__':
-    if len(sys.argv) == 2:
-        port = int(sys.argv[1])
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind(('', port))
-        s.listen(1)
-        conn, addr = s.accept()
+    try:
+        if len(sys.argv) == 2:
+            port = int(sys.argv[1])
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.bind(('', port))
+            s.listen(1)
+            listener = keyboard.Listener(
+                        on_press=on_press,
+                        on_release=on_release)
+            listener.start()
 
-        print('Connected by', addr)
+            while 1:
+                print("In while loop")
+                conn, addr = s.accept()
+                devices[addr] = conn
+                print('Connected by', addr)
 
-        while 1:
-            data = conn.recv(1024)
-            if not data:
-                break
-            else:
-                print(repr(data))
-        conn.close()
-    else:
-        print('Error starting server. Invalid arguments used\nreqd: host_name, port_number')
+                if not focused:
+                    focused = addr
+
+                data = conn.recv(1024)
+                if data:
+                    print(repr(data))
+                conn.sendall('Ready to calibrate')
+
+            conn.close()
+        else:
+            print('Error starting server. Invalid arguments used\nreqd: port_number')
+    except KeyboardInterrupt:
+        s.close()
+    except Exception as e:
+        traceback.print_exc();
+        s.close()
