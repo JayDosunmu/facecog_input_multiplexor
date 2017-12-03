effe697f04fd3712307ea540d1110a1807177623
TheBlasianTJ <tjdosunmu@gmail.com>
2017-12-02 22:50:40 -0500

    setup client-server model

--- client.py
+++ client.py
@@ -1,6 +1,25 @@
 # keyboard simulator
 import pyautogui
 import socket
+import sys
 
 
+def register_device(host, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
 
+    return s
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 3:
+        s = register_device(sys.argv[1], sys.argv[2])
+        s.sendall('Connected to Device')
+
+        while 1:
+            data = s.recv(1024)
+            print("data received: " + repr(data))
+    else:
+        print('ERROR: Incorrect number of arguments used. ' +
+              'You must pass in the HOST and PORT of the endpoint to connect to\n' +
+              'Example: python client.py host_name port_number')
--- server.py
+++ server.py
@@ -12,3 +12,22 @@ conn, addr = socket.accept()
 devices[addr] = Device(addr, conn)
 
 
+
+
+if __name__ == '__main__':
+    host = ''
+    post = 12345
+    s = socket.socket(stream.AF_INET, socket.SOCK_STREAM)
+    s.bind((host, post))
+    s.listen(1)
+    conn, addr = s.accept()
+
+    print('Connected by', addr)
+
+    while 1:
+        data = conn.recv(1024)
+        if not data:
+            break
+        else:
+            print(repr(data))
+    conn.close()
