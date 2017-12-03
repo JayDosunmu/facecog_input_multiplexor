4c9ba3e1c54ebd6378d3dd620f47dc0826af55ae
TheBlasianTJ <tjdosunmu@gmail.com>
2017-12-02 23:02:51 -0500

    Set up way to pass in hostname and port as arguments

--- server.py
+++ server.py
@@ -1,9 +1,12 @@
 # signal mux
 import socket
+import sys
+
 
 class Device:
     def __init__(self, addr, conn):
         pass
+
 
 devices = {}
 
@@ -12,22 +15,23 @@ conn, addr = socket.accept()
 devices[addr] = Device(addr, conn)
 
 
-
-
 if __name__ == '__main__':
-    host = ''
-    post = 12345
-    s = socket.socket(stream.AF_INET, socket.SOCK_STREAM)
-    s.bind((host, post))
-    s.listen(1)
-    conn, addr = s.accept()
+    if len(sys.argv) == 3:
+        host = sys.argv[1]
+        post = int(sys.argv[2])
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((host, post))
+        s.listen(1)
+        conn, addr = s.accept()
 
-    print('Connected by', addr)
+        print('Connected by', addr)
 
-    while 1:
-        data = conn.recv(1024)
-        if not data:
-            break
-        else:
-            print(repr(data))
-    conn.close()
+        while 1:
+            data = conn.recv(1024)
+            if not data:
+                break
+            else:
+                print(repr(data))
+        conn.close()
+    else:
+        print('Error starting server. Invalid arguments used\nreqd: host_name, port_number')
